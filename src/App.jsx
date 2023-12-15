a8febd2df528aa0b5d0b1e16f58abc3c0dc995a7
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2023-12-15 18:35:46 +0100

    add state gameTurns in app component and pass it to gameBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { useState } from 'react'
 import Player from './components/player'
 import GameBoard from './components/GameBoard'
+import Log from './components/Log'
 
 function App() {
 	const [activePlayer, setActivePlayer] = useState('X')
+	const [gameTurns, setGameTurns] = useState([])
 
 	function handleSelectSquare() {
 		setActivePlayer(curActivePlayer => (curActivePlayer === 'X' ? 'O' : 'X'))
+		setGameTurns(prevTurns => {
+			let currentPlayer = 'X'
+
+			if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+				currentPlayer = 'O'
+			}
+			const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns]
+
+			return updatedTurns
+		})
 	}
 	return (
 		<main>
-			<div id="game-container" >
+			<div id="game-container">
 				<ol id="players" className="highlight-player">
 					<Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
 					<Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
 				</ol>
-				<GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+				<GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+				<Log />
 			</div>
 		</main>
 	)
