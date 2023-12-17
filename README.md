cf2949bc417d4141f75941a4becb57f1c39a1d34
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2023-12-17 19:49:19 +0100

    add disabled button and remove state form app component

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Tic-tac-toe-game
+
+My second react project.
+
+In this project which is inspired by react course
+React - The Complete Guide 2023 (incl. React Router & Redux)
+by Maximilian Schwarzmüller
+
+In this project I'll be creating a game and learn basisc of react.
--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import Player from './components/player'
 import GameBoard from './components/GameBoard'
 import Log from './components/Log'
 
+function deriveActivePlayer(gameTurns) {
+	let currentPlayer = 'X'
+
+	if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+		currentPlayer = 'O'
+	}
+	return currentPlayer
+}
+
 function App() {
-	const [activePlayer, setActivePlayer] = useState('X')
+	// const [activePlayer, setActivePlayer] = useState('X')
 	const [gameTurns, setGameTurns] = useState([])
-
-	function handleSelectSquare() {
-		setActivePlayer(curActivePlayer => (curActivePlayer === 'X' ? 'O' : 'X'))
+	const activePlayer = deriveActivePlayer(gameTurns)
+	function handleSelectSquare(rowIndex, colIndex) {
+		// setActivePlayer(curActivePlayer => (curActivePlayer === 'X' ? 'O' : 'X'))
 		setGameTurns(prevTurns => {
-			let currentPlayer = 'X'
-
-			if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-				currentPlayer = 'O'
-			}
+			const currentPlayer = deriveActivePlayer(prevTurns)
 			const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns]
 
 			return updatedTurns
@@ -28,8 +33,8 @@ function App() {
 					<Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
 				</ol>
 				<GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
-				<Log />
 			</div>
+			<Log turns={gameTurns} />
 		</main>
 	)
 }
