3000f42f2a1eaef8c91709ffea8a288fc7489d3f
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2023-12-17 21:19:53 +0100

    add name to winning board

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,10 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
 	// const [activePlayer, setActivePlayer] = useState('X')
+	const [players, setPlayers] = useState({
+		X: 'Player 1',
+		O: 'Player 2',
+	})
 	const [gameTurns, setGameTurns] = useState([])
 	const activePlayer = deriveActivePlayer(gameTurns)
 
@@ -43,7 +47,7 @@ function App() {
 		const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
 
 		if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
-			winner = firstSquareSymbol
+			winner = players[firstSquareSymbol]
 		}
 	}
 
@@ -59,12 +63,21 @@ function App() {
 	function handleRemach() {
 		setGameTurns([])
 	}
+
+	function changePlayerName(symbol, newName) {
+		setPlayers(prevPlayers => {
+			return {
+				...prevPlayers,
+				[symbol]: newName,
+			}
+		})
+	}
 	return (
 		<main>
 			<div id="game-container">
 				<ol id="players" className="highlight-player">
-					<Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-					<Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+					<Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} onChangeName={changePlayerName} />
+					<Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} onChangeName={changePlayerName} />
 				</ol>
 				{(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRemach} />}
 				<GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
 	const [playerName, setPlayerName] = useState(initialName)
 	const [isEditing, setIsEditing] = useState(false)
 
 	const handleClickEdit = () => {
 		setIsEditing(editing => !editing)
+
+		if (isEditing) {
+			onChangeName(symbol, playerName)
+		}
 	}
 
 	function changeName(event) {
